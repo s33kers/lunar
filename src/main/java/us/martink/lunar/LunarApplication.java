5c31c1a204b1719da0588b2259f9d6dcf2e9cf22
tadasmartinkus <Tadas.Martinkus@consult.luminorgroup.com>
2019-09-05 20:18:09 +0300

    init springboot project build with gradle

---
+++ src/main/java/us/martink/lunar/LunarApplication.java
@@ -0,0 +1,11 @@
+package us.martink.lunar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LunarApplication.class, args);
+    }
+}
