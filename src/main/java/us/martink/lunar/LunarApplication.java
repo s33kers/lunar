41e9d146a3da6cfcca6b29fa2a0c5c87a70cd768
tadasmartinkus <Tadas.Martinkus@consult.luminorgroup.com>
2019-09-08 18:27:23 +0300

    registered requestcontextfilter for application

--- src/main/java/us/martink/lunar/LunarApplication.java
+++ src/main/java/us/martink/lunar/LunarApplication.java
@@ -2,10 +2,17 @@ package us.martink.lunar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import us.martink.lunar.context.RequestContextFilter;
 
 @SpringBootApplication
 public class LunarApplication {
     public static void main(String[] args) {
         SpringApplication.run(LunarApplication.class, args);
+    }
+
+    @Bean
+    public RequestContextFilter requestContext() {
+        return new RequestContextFilter();
     }
 }
